c657313ddcc6803ee215cd69c69f6931723398c7
Mariano Luis Uymatiao <wizfactormail@gmail.com>
2013-06-20 22:00:59 +0800

    Initial jQuery fade-in animation of buttons has been added. Separated Javascript and CSS into their own files and folders. jQuery library and background image are included to allow offline work.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+	$("#loginButton").hide();
+	$("#registerButton").hide();
+
+	$("#loginButton").delay(500).fadeIn(600);
+	$("#registerButton").delay(500).fadeIn(600);
+
+	$("#registerButton").click(function() {
+		$("#registerButton").animate({
+			
+		});
+	});	
+});
